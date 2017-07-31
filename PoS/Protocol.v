ad9f37ea75c6ab6625df95c55bef10af75cd10bc
George Pîrlea <george@dranov.ro>
2017-07-31 13:43:56 +0100

    Add Packets with eq + incomplete net semantics

--- PoS/Protocol.v
+++ PoS/Protocol.v
@@ -2,7 +2,7 @@ From mathcomp.ssreflect
 Require Import ssreflect ssrbool ssrnat eqtype ssrfun seq.
 From mathcomp
 Require Import path.
-Require Import Eqdep pred prelude idynamic ordtype pcm finmap unionmap heap coding. 
+Require Import Eqdep pred prelude idynamic ordtype pcm finmap unionmap heap coding.
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
@@ -16,17 +16,17 @@ Inductive Message :=
   | Addr of nid & peers_t
   | Connect of nid.
 
-Record Transmission := mkT {to: nid; msg: Message}.
-Definition NullTransmission := mkT 0 NullMsg.
+Record Packet := mkP {src: nid; dst: nid; msg: Message}.
+Definition NullPacket := mkP 0 0 NullMsg.
 
-Definition ToSend := seq Transmission.
-Definition emitZero : ToSend := [:: NullTransmission].
-Definition emitOne (trans : Transmission) : ToSend := [:: trans].
-Definition emitMany (trsms : ToSend) := trsms.
+Definition ToSend := seq Packet.
+Definition emitZero : ToSend := [:: NullPacket].
+Definition emitOne (packet : Packet) : ToSend := [:: packet].
+Definition emitMany (packets : ToSend) := packets.
 
-Definition emitOneToOne (to : nid) (msg : Message) : ToSend := [:: mkT to msg].
-Definition emitManyToOne (to : nid) (msgs : seq Message) : ToSend :=
-  [seq (mkT to msg) | msg <- msgs].
+Definition emitOneToOne (from to : nid) (msg : Message) := [:: mkP from to msg].
+Definition emitManyToOne (from to : nid) (msgs : seq Message) :=
+  [seq (mkP from to msg) | msg <- msgs].
 
 
 Section Node. (* Node behaviour *)
@@ -41,7 +41,7 @@ Definition Init (n : nid) : State := Node n [:: n].
 Lemma peers_uniq_init (n : nid) : uniq [::n]. Proof. done. Qed.
   
 
-Definition upd : State -> Message -> (State * ToSend) :=
+Definition updS : State -> Message -> (State * ToSend) :=
   fun (st: State) (msg: Message) =>
     match st with
     | Node n prs =>
@@ -49,21 +49,21 @@ Definition upd : State -> Message -> (State * ToSend) :=
       | Connect peer => pair (Node n (undup (peer :: prs))) emitZero
       | Addr _ knownPeers =>
         let: newP := [seq x <- knownPeers | x \notin prs] in
-        let: connects := [seq mkT p (Connect n) | p <- newP] in
+        let: connects := [seq mkP n p (Connect n) | p <- newP] in
           pair (Node n (undup (prs ++ newP))) (emitMany(connects))
       | _ => pair st emitZero
       end
     end.
 
 Lemma upd_id_constant :
-  forall (s1 : State) (m : Message), let: s2 := (upd s1 m).1 in
+  forall (s1 : State) (m : Message), let: s2 := (updS s1 m).1 in
     id s1 = id s2.
 Proof.
 case=> n1 p1 []; by [].
 Qed.
 
 Lemma upd_peers_uniq :
-  forall (s1 : State) (m : Message), let: s2 := (upd s1 m).1 in
+  forall (s1 : State) (m : Message), let: s2 := (updS s1 m).1 in
     uniq (peers s1) -> uniq (peers s2).
 Proof.
 case=> n1 p1 [].
@@ -79,7 +79,7 @@ Qed.
 
 Inductive step (s1 s2 : State) : Prop :=
 | Idle of s1 = s2
-| RcvMsg (m : Message) of (s2 = (upd s1 m).1).
+| RcvMsg (m : Message) of (s2 = (updS s1 m).1).
 
 Lemma id_constant :
   forall (s1 s2 : State),
@@ -87,8 +87,8 @@ Lemma id_constant :
 Proof.
 move=> s1 s2.
 case.
-- move=> eq. rewrite eq. by [].
-  - move=> m Us. rewrite Us. apply upd_id_constant.
+- move=> eq. rewrite eq. by  [].
+- move=> m Us. rewrite Us. apply upd_id_constant.
 Qed.
 
 Lemma peers_uniq :
@@ -97,8 +97,8 @@ Lemma peers_uniq :
 Proof.
 move=> s1 s2 UniqP1.
 case.
-  - move=> eq. rewrite -eq. by [].
-  - move=> m Us. rewrite Us. apply upd_peers_uniq. by [].
+- move=> eq. rewrite -eq. by [].
+- move=> m Us. rewrite Us. apply upd_peers_uniq. by [].
 Qed.
 
 End Node.
--- PoS/Semantics.v
+++ PoS/Semantics.v
@@ -3,9 +3,58 @@ Require Import ssreflect ssrbool ssrnat eqtype ssrfun seq.
 From mathcomp
 Require Import path.
 Require Import Eqdep pred prelude idynamic ordtype pcm finmap unionmap heap coding. 
+Require Import Protocol.
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
 
 (* Network semantics *)
+Section PacketEqType.
+
+Fixpoint eqpkt a b :=
+  ((src a) == (src b)) && ((dst a) == (dst b)) &&
+  match (msg a), (msg b) with
+  | NullMsg, NullMsg => true
+  | NullMsg, _ => false
+  | Addr idA prsA, Addr idB prsB => (idA == idB) && (prsA == prsB)
+  | Addr _ _, _ => false
+  | Connect idA, Connect idB => (idA == idB)
+  | Connect _, _ => false
+  end.
+
+Lemma eqpktP : Equality.axiom eqpkt.
+Proof.
+Admitted.
+
+Canonical Packet_eqMixin := EqMixin eqpktP.
+Canonical Packet_eqType := EqType Packet Packet_eqMixin.
+
+End PacketEqType.
+
+
+Definition PacketSoup := seq Packet.
+
+Definition StateMap := union_map [ordType of nid] State.
+
+Record World :=
+  mkW {
+    localState : StateMap;
+    inFlightMsgs : PacketSoup;
+  }.
+
+Print Equality.sort.
+
+Inductive reliable_step (w w' : World) : Prop :=
+| step_msg (p : Packet) (st' : State) (ms : ToSend) of
+      p \in (inFlightMsgs w) &
+      match (find (dst p) (localState w)) with
+      | Some(st) => updS st (msg p) = (st', ms)
+      | _ => false
+      end &
+
+      reliable_step w
+        (mkW
+           (upd (dst p) st' (localState w))
+           (ms ++ rem p (inFlightMsgs w))
+        ).
