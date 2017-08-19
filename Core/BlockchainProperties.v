7cd8ca4012e695a29cd7d36425fe7c146a2f7f70
Ilya Sergey <i.sergey@ucl.ac.uk>
2017-08-19 20:49:37 +0100

    reworked definition of fork etc

--- Core/BlockchainProperties.v
+++ Core/BlockchainProperties.v
@@ -10,15 +10,53 @@ Unset Printing Implicit Defensive.
 
 Section BlockchainOrder.
 
-Definition is_prefix (bc bc' : Blockchain) :=
+(* Strict version of the prefix *)
+Definition is_strict_prefix {T: eqType} (bc bc' : seq T) :=
+  exists b bc1, bc' = bc ++ (b :: bc1).
+
+Notation "'[' bc1 '<<<' bc2 ']'" := (is_strict_prefix bc1 bc2).
+
+Lemma isp_mt {T: eqType} (bc : seq T) : ~ is_strict_prefix bc [::].
+Proof. by case=>b[bc1]; case: bc. Qed.
+
+(* The corresponding checker *)
+Fixpoint sprefixb {T: eqType} (s1 s2 : seq T) :=
+  if s2 is y :: s2' then
+    if s1 is x :: s1' then (x == y) && (sprefixb s1' s2')
+    else true
+  else false.         
+
+Lemma sprefixP {T: eqType} (bc1 bc2 : seq T):
+  reflect [bc1 <<< bc2] (sprefixb bc1 bc2).
+Proof.
+elim: bc2 bc1=>//=[|b2 bc2 Hi/=]bc1.
+- case:bc1=>//=[|b1 bc1]; constructor 2=>//; apply: isp_mt.
+case: bc1=>//[|b1 bc1]/=; first by constructor 1; exists b2, bc2.  
+case X: (b1 == b2)=>/=; last first.
+- constructor 2=>[[p]][bc']; rewrite cat_cons; case=>Z; subst b2.
+  by rewrite eqxx in X.
+- move/eqP: X=>X; subst b2.
+case: Hi=>H; [constructor 1|constructor 2].
+- by case:H=>x[xs]->; exists x, xs; rewrite cat_cons.  
+case=>x[xs]; rewrite cat_cons; case=>Z; subst bc2; apply: H.
+by exists x, xs.
+Qed.
+
+(* Decidable fork *)
+Definition fork {T: eqType} (bc1 bc2 : seq T) :=
+  ~~[|| sprefixb bc1 bc2, sprefixb bc2 bc1 | bc1 == bc2].
+
+
+(* Non-strict prefix *)
+Definition is_prefix {T :eqType} (bc bc' : seq T) :=
   exists bc1, bc' = bc ++ bc1.
 
 Notation "'[' bc1 '<<=' bc2 ']'" := (is_prefix bc1 bc2).
 
-Lemma bc_pre_refl bc : [bc <<= bc].
+Lemma bc_pre_refl {T :eqType} (bc : seq T) : [bc <<= bc].
 Proof. by exists [::]; rewrite cats0. Qed.
 
-Lemma bc_pre_trans bc1 bc2 bc3 :
+Lemma bc_pre_trans {T :eqType} (bc1 bc2 bc3 : seq T) :
   [bc1 <<= bc2] -> [bc2 <<= bc3] -> [bc1 <<= bc3].
 Proof.
 case=> a1 H1 [a2] H2; subst bc2.
@@ -31,7 +69,7 @@ Proof.
 by case=>ext=>eq; rewrite eq; apply CFR_ext.
 Qed.
 
-Lemma bc_prefix_mt bc : [bc <<= [::]] -> bc == [::].
+Lemma bc_prefix_mt {T :eqType} (bc : seq T) : [bc <<= [::]] -> bc == [::].
 Proof. by case: bc=>//b bc[x]. Qed.
  
 Fixpoint prefixb {T: eqType} (s1 s2 : seq T) :=
@@ -40,10 +78,11 @@ Fixpoint prefixb {T: eqType} (s1 s2 : seq T) :=
     else true
   else s1 == [::].         
 
-Lemma bc_prefixb_mt (bc : Blockchain) : prefixb bc [::] -> bc == [::].
+Lemma bc_prefixb_mt {T :eqType} (bc : seq T) : prefixb bc [::] -> bc == [::].
 Proof. by case: bc=>//b bc[x]. Qed.
 
-Lemma prefixP bc1 bc2: reflect [bc1 <<= bc2] (prefixb bc1 bc2).
+Lemma prefixP {T :eqType} (bc1 bc2 : seq T):
+  reflect [bc1 <<= bc2] (prefixb bc1 bc2).
 Proof.
 elim: bc2 bc1=>//=[|b2 bc2 Hi/=]bc1.
 - case B: (prefixb bc1 [::]); [constructor 1|constructor 2].
@@ -58,51 +97,56 @@ case: Hi=>H; [constructor 1|constructor 2].
 - by case:H=>x->; exists x; rewrite cat_cons.  
 case=>t; rewrite cat_cons; case=>Z; subst bc2; apply: H.
 by exists t.
-Qed.                                               
+Qed.
 
+Lemma prb_equiv {T: eqType} (bc1 bc2 : seq T) :
+  prefixb bc1 bc2 = (bc1 == bc2) || (sprefixb bc1 bc2).
+Proof.
+apply/Bool.eq_iff_eq_true; split.
+- move/prefixP=>[x]->; case: x=>[|x xs]; first by rewrite cats0 eqxx.
+  by apply/orP; right; apply/sprefixP; exists x, xs.
+(* TODO: george, please, finish this as an exercise! *)
+admit.
+Admitted.
+  
 End BlockchainOrder.
 
 Notation "'[' bc1 '<<=' bc2 ']'" := (is_prefix bc1 bc2).
+Notation "'[' bc1 '<<<' bc2 ']'" := (is_strict_prefix bc1 bc2).
 
 Section Forks.
 
-Definition fork (bc bc' : Blockchain) : Prop :=
-  ~[bc <<= bc'] /\  ~[bc' <<= bc].
-
-Lemma bc_fork_neq bc bc' :
-  fork bc bc' -> bc != bc' = true.
+Lemma bc_fork_neq {T: eqType} (bc bc' : seq T) :
+  fork bc bc' -> bc != bc'.
 Proof.
-elim=> H1 H2. elim: bc H1 H2=>[|x xs Hi] H1 H2.
-- by contradict H1; exists bc'.
-elim: bc' H1 H2 Hi=>[|y ys Hi H1 H2 Hi'].
-- by [].
-case B: (x == y); rewrite eqseq_cons Bool.negb_true_iff Bool.andb_false_iff.
-- right. move/eqP in B. subst x. rewrite/is_prefix in H2.
-  apply/negbTE/negP; move/eqP=>G. apply: H2.
-  by exists [::]; rewrite cats0 G.
-- by left.
+move=>H; apply/negbT/negP=>Z; move/eqP:Z=>Z; subst bc'.
+by case/orP: H; right; rewrite orbC eqxx. 
 Qed.
 
-Lemma bc_fork_sym bc bc' :
+Lemma bc_fork_sym {T: eqType} (bc bc' : seq T) :
   fork bc bc' -> fork bc' bc.
 Proof.
-rewrite/fork. elim. move=>nbb' nb'b. split; by [].
+by rewrite/fork; rewrite eq_sym orbCA.
 Qed.
 
-Lemma bc_fork_prefix a b c :
+Lemma bc_fork_prefix {T: eqType} (a b c : seq T) :
   fork a b -> [b <<= c] -> fork a c.
 Proof.
-rewrite/fork. elim=>H2 H1[x] H3. subst c.
-elim: x b H1 H2=>[|x xs Hi] b H1 H2.
-- by rewrite cats0.
-rewrite -cat_rcons. apply: Hi=>H.
-- by apply: H1; case: H=>z->; rewrite cat_rcons; eexists _.
-- rewrite/is_prefix in H H1 H2.
-case: H=>z. elim/last_ind: z=>[|zs z Hi].
-- by rewrite cats0=>Z; subst a; apply: H1; exists [:: x]; rewrite cats1.
-rewrite -rcons_cat=>/eqP. rewrite eqseq_rcons. move/andP=>[/eqP Z]/eqP Z'.
-by subst x b; apply: H2; exists zs.
-Qed.
+move/orP=>H1 H2; apply/negP=>/orP H3; apply: H1.
+case:H3.
+Admitted.  
+(* TODO: redo me for the new fork definition! *)
+
+(* rewrite/fork. elim=>H2 H1[x] H3. subst c. *)
+(* elim: x b H1 H2=>[|x xs Hi] b H1 H2. *)
+(* - by rewrite cats0. *)
+(* rewrite -cat_rcons. apply: Hi=>H. *)
+(* - by apply: H1; case: H=>z->; rewrite cat_rcons; eexists _. *)
+(* - rewrite/is_prefix in H H1 H2. *)
+(* case: H=>z. elim/last_ind: z=>[|zs z Hi]. *)
+(* - by rewrite cats0=>Z; subst a; apply: H1; exists [:: x]; rewrite cats1. *)
+(* rewrite -rcons_cat=>/eqP. rewrite eqseq_rcons. move/andP=>[/eqP Z]/eqP Z'. *)
+(* by subst x b; apply: H2; exists zs. *)
 
 Axiom btChain_fork :
   forall (bt : BlockTree) (bc : Blockchain) (b : Block),
