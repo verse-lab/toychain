687a584a8e257274e64ebe22fef19ce72124de32
Ilya Sergey <i.sergey@ucl.ac.uk>
2017-10-03 19:38:03 +0100

    more comments

--- Core/Blockchain.v
+++ Core/Blockchain.v
@@ -1686,8 +1686,7 @@ move=>V Vh Hib Vl Vhl Hil Hg Hg' Gt P Ec Cont.
    a blockchain larger than it already had and has. In other words,
    either this block is already in the cbt, or now, but isn't
    contributing, due to goodness of the final tree. *)
-case Nb: (#b \in dom cbt); first by rewrite /btExtend Nb in Cont; apply: CFR_nrefl Cont.
-case: (btExtend_good_split V Vh Hib Hg (negbT Nb) Hg')=>cs1[cs2][Eg][Eg'].
+
 
 (* Ok, somebody minted a block, which does not contribute to cbt' *)
 (* blockchain, only to the local one. *)
@@ -1708,6 +1707,18 @@ Use the chain-splitting lemma `btExtend_good_split` to inspect
 individual elements in cbt.
 
 *)
+
+case Nb: (#b \in dom cbt); first by rewrite /btExtend Nb in Cont; apply: CFR_nrefl Cont.
+case: (btExtend_good_split V Vh Hib Hg (negbT Nb) Hg')=>cs1[cs2][Eg][Eg'].
+
+have Gb: good_chain (compute_chain (btExtend bt b) b).
+ (* Use `btExtend_mint_ext Vl Vhl Hil`.  *).
+ (* follows from the fact that we attach to btChain *)
+
+(* Okay, this is no necessarily (btChain (btExtend cbt b)), but it is
+   the only chain added to cbt, and it is not larger than (btChain
+   (btExtend cbt b)), hence derive the contradiction. *)
+
 
 Admitted.
 
