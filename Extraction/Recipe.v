98395ecea358447885ab2e8775adbde3cd370f1a
George Pîrlea <george@dranov.ro>
2019-02-19 21:14:57 +0000

    Compiles and runs! (makes no sense yet, though)

--- Extraction/Recipe.v
+++ Extraction/Recipe.v
@@ -14,6 +14,18 @@ Extraction Inline ssrbool.SimplPred.
 (* ordinals are nat, and we want to extract nat to int *)
 Extract Inductive nat => int [ "0" "succ" ] "(fun fO fS n -> if n=0 then fO () else fS (n-1))".
 
+Extract Constant ProofOfWork.hashT =>
+  "(fun tx -> 0)".
+
+Extract Constant ProofOfWork.hashB =>
+  "(fun blk -> 0)".
+
+(* coq_Blockchain -> coq_TxPool -> coq_Timestamp -> *)
+(* (coq_TxPool * coq_VProof) option *)
+
+Extract Constant ProofOfWork.genProof =>
+  "(fun bc txp ts -> None)".
+
 Cd "Extraction/src/toychain".
 Separate Extraction
   ProtocolImpl.procMsg
--- Extraction/src/consensus.ml
+++
@@ -1,8 +0,0 @@
-open Forests
-open Protocol
-
-module Addr = Address.Addr
-module ConsensusParams = Impl.ProofOfWork
-module ForestImpl = Forests (ConsensusParams)
-module ProtocolImpl = Protocol (ConsensusParams) (ForestImpl) (Addr)
-
