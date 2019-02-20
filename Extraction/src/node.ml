eb4ba01a7cf45253dd0afdc191847e94a866d8eb
George Pîrlea <george@dranov.ro>
2019-02-20 09:52:30 +0000

    Slightly better example
    Can't yet actually look into the blocks because the type is not
    properly initialised.
    
    changes. Lines starting

--- Extraction/src/node.ml
+++ Extraction/src/node.ml
@@ -1,17 +1,25 @@
 open Forests
 open Protocol
+open Util
 
 module Addr = Address.Addr
 module ConsensusParams = Impl.ProofOfWork
 module ForestImpl = Forests (ConsensusParams)
 module ProtocolImpl = Protocol (ConsensusParams) (ForestImpl) (Addr)
 
-let state = ref (ProtocolImpl.coq_Init 0);;
+let node_id = 0
+let st = ref (ProtocolImpl.coq_Init node_id);;
+
+open ForestImpl
 
 let main () =
   let argv = Array.to_list Sys.argv in
   let args = List.tl argv in
-  Printf.printf "You are node %s\n" (string_of_int (!state).id)
+  let blocks = all_blocks !st.blockTree in
+  Printf.printf "Your blocktree has %s block(s)!\n"
+    (string_of_int (List.length blocks)) ;;
+  Printf.printf "You are node %s\n" (string_of_int !st.id) ;;
+
 
 let () = main ()
 
